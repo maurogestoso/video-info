6d40d24ed84bb9cfdeb9c5541b1039bd098585e1
Mauro Gestoso <maurogestoso@gmail.com>
2018-01-13 13:48:55 +0000

    Implement basic file upload form

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const electron = require("electron");
+document.querySelector("form").addEventListener("submit", event => {
+  event.preventDefault();
+  const file = event.target.elements.fileInput.files[0];
+  console.log(file);
+});
